0ffe9dbb9a67c92fab1a46f26af8720b7df088e4
HassaneSass07 <sasshassane02@gmail.com>
2025-06-02 07:16:01 +0100

    rocknroll

--- remove_spaces.c
+++ remove_spaces.c
@@ -4,17 +4,8 @@ int main() {
     char str[100], new_str[100];
     int i = 0, j = 0;
 
-    // Lecture de la chaîne 
-    scanf("%[^\n]", str); vvv
-
-    // Suppression des espaces
-    while (str[i] != '\0') {
-        if (str[i] != ' ') {
-            new_str[j++] = str[i];
-        }
-        i++;
-    }
-    new_str[j] = '\0';
+    // Lecture de la chaîne
+    scanf("%[^\n]", str);
 
     // Affiche la chaîne sans espaces
     printf("%s\n", new_str);
