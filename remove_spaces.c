1ca544f1825225a162ef2ea420c31639dea03e00
HassaneSass07 <sasshassane02@gmail.com>
2025-06-02 07:32:45 +0100

    messsi

--- remove_spaces.c
+++ remove_spaces.c
@@ -7,6 +7,15 @@ int main() {
     // Lecture de la chaîne
     scanf("%[^\n]", str);
 
+    // Suppression des espaces
+    while (str[i] != '\0') {
+        if (str[i] != ' ') {
+            new_str[j++] = str[i];
+        }
+        i++;
+    }
+    new_str[j] = '\0';
+
     // Affiche la chaîne sans espaces
     printf("%s\n", new_str);
     return 0;
